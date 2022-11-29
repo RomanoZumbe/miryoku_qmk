0ef30bf7991a53e0e91da89fe4c483a603b76ef5
Romano Zumbé <rzumbe@gmail.com>
2022-11-29 20:56:27 +0100

    more doubt

---
+++ users/manna-harbour_miryoku/localization/custom_keycodes.h
@@ -0,0 +1,4 @@
+enum custom_keycodes {
+    PLACEHOLDER = SAFE_RANGE,  // can always be here (4 bytes)
+    CYCLE                      // cycle through first BASE_LAYERS (62 bytes)
+}
--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -4,7 +4,7 @@
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
-
+#include "./localization/custom_keycodes.h"
 #include "../../quantum/keymap_extras/keymap_german.h"
 #include "manna-harbour_miryoku.h"
 
@@ -17,6 +17,7 @@ enum {
 MIRYOKU_LAYER_LIST
 #undef MIRYOKU_X
 };
+
 
 void u_td_fn_boot(qk_tap_dance_state_t *state, void *user_data) { \
   if (state->count == 2) {
@@ -92,3 +93,20 @@ combo_t key_combos[COMBO_COUNT] = {
   COMBO(thumbcombos_fun, KC_APP)
 };
 #endif
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // handling this once instead of in each keycode uses less program memory.
+    if ((keycode >= SAFE_RANGE) && !(record->event.pressed)) {
+        return false;
+    }
+
+    switch (keycode) {
+        case CYCLE:
+            register_code(DE_DLR);
+            break;
+    }
+
+    // this uses less memory than returning in each case.
+    return keycode < SAFE_RANGE;
+};
