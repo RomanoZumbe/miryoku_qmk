9d33c61859aa4678744b44e036ed64cb57519b17
Romano Zumbe <rzumbe@gmail.com>
2022-12-03 00:02:54 +0100

    custom keycode

--- users/manna-harbour_miryoku/localization/custom_keycodes.h
+++ users/manna-harbour_miryoku/localization/custom_keycodes.h
@@ -1,4 +1,5 @@
 enum custom_keycodes {
     PLACEHOLDER = SAFE_RANGE,  // can always be here (4 bytes)
-    CYCLE                      // cycle through first BASE_LAYERS (62 bytes)
+    CYCLE,                      // cycle through first BASE_LAYERS (62 bytes)
+    SS_CRYPT
 };
--- users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -146,7 +146,7 @@ LGUI_T(KC_Q),              KC_W,              KC_E,              KC_R,          
 KC_A,      KC_S,      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      DE_QUOT,   \
 LT(U_BUTTON,KC_Y), LALT_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              DE_COMM,           DE_DOT,    DE_SLSH,\
 U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_ENT),  LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_SPC),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,              U_NP,\
-KC_Q
+SS_CRYPT
 
 
 #define MIRYOKU_ALTERNATIVES_TAP_AZERTY_FLIP \
