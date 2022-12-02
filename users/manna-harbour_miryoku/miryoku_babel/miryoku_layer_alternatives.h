4156aa7ec8af5c26e193477d728b942fc4870068
Romano Zumbe <rzumbe@gmail.com>
2022-12-02 23:30:33 +0100

    corrections german layout, banime adjustments

--- users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -143,8 +143,8 @@ KC_Q
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTZ \
 LGUI_T(KC_Q),              KC_W,              KC_E,              KC_R,              KC_T,              KC_Z,              KC_U,              KC_I,              ALGR_T(KC_O),              LGUI_T(KC_P),              \
-KC_A,      KC_S,      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      KC_QUOT,   \
-LT(U_BUTTON,KC_Y), LALT_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           KC_DOT,    DE_SLSH,\
+KC_A,      KC_S,      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      DE_QUOT,   \
+LT(U_BUTTON,KC_Y), LALT_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              DE_COMM,           DE_DOT,    DE_SLSH,\
 U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_ENT),  LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_SPC),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,              U_NP,\
 KC_Q
 
