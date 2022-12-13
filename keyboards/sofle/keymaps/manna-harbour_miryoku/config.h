67d57f98662f122b666b6c5f256e3b63f44fc9d7
Romano Zumbe <rzumbe@gmail.com>
2022-12-13 14:45:47 +0100

    Update config.h

--- keyboards/sofle/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/sofle/keymaps/manna-harbour_miryoku/config.h
@@ -12,12 +12,14 @@
      K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19,\
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29,\
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39,\
-     K38\
+     K38, K39, K40, K41, K42, K43, K44, K45, K46, K47, \
+     K48, K49, K50, K51, K52, K53, K54, K55, \
+     K56, K57, K58, K59
 )\
 LAYOUT(\
-K38, XXX, XXX, XXX, XXX, XXX,                XXX, XXX, XXX, XXX, XXX, XXX,\
-XXX, K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, XXX,\
-XXX, K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, XXX,\
-XXX, K20, K21, K22, K23, K24, XXX,      XXX, K25, K26, K27, K28, K29, XXX,\
-          XXX, XXX, K32, K33, K34,      K35, K36, K37, XXX, XXX\
+K48, K38, K39, K40, K41, K42,                K43, K44, K45, K46, K47, K52,\
+K49, K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, K53,\
+K50, K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, K54,\
+K51, K20, K21, K22, K23, K24, XXX,      XXX, K25, K26, K27, K28, K29, K55,\
+          K56, K57, K32, K33, K34,      K35, K36, K37, K58, K59\
 )
