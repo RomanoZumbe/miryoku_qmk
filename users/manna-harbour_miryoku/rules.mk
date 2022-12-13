41fc1498654132c74fd356c673b1f0c70654c10f
Romano Zumbe <rzumbe@gmail.com>
2022-12-13 16:31:22 +0100

    Update rules.mk

--- users/manna-harbour_miryoku/rules.mk
+++ users/manna-harbour_miryoku/rules.mk
@@ -2,7 +2,7 @@
 # https://github.com/manna-harbour/miryoku
 
 MOUSEKEY_ENABLE = yes
-EXTRAKEY_ENABLE = yes
+# EXTRAKEY_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 # CAPS_WORD_ENABLE = yes
